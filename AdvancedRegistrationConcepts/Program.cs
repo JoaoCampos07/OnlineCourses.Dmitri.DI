07f886f2c573c700f5d60a0e559e86bb53b56a9a
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-09 15:47:30 +0100

    [Class3] + Add: problem : We want to create a instance of obj in another class, depending in some conditions, using DI !

--- AdvancedRegistrationConcepts/Program.cs
+++ AdvancedRegistrationConcepts/Program.cs
@@ -4,50 +4,35 @@ using System;
 
 namespace AdvancedRegistrationConcepts
 {
-    public class Service
+    public class Entity
     {
-        public string DoSomething(int value)
-            => $"I have a {value}";
-    }
+        private static Random random = new Random();
+        private int number;
 
-    public class DomainObject
-    {
-        private Service service;
-        private int value;
-
-        public delegate DomainObject Factory(int value); // we specify params that are NOT auto injected by AutoFac 
-
-        public DomainObject(Service service, int value)
+        public Entity()
         {
-            this.service = service;
-            this.value = value;
+            number = random.Next();
         }
 
         public override string ToString()
-        => this.service.DoSomething(this.value);
+         => "test" + number.ToString();
+    }
+
+    public class ViewModel
+    {
+        // We want to construct a instante of Entity using DI
+        public void Method()
+        {
+
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var builder = new ContainerBuilder();
-            builder.RegisterType<Service>();
-            builder.RegisterType<DomainObject>();
 
-            var container = builder.Build();
-            // Passed Arguments to a obj when we are resolving it.
-            // 1 Way : 
-            // ( I want to provide a value right here )
-
-            //var obj = container.Resolve<DomainObject>(new PositionalParameter(1, 42)); // 1 -> because the parameter is at position 1 in the constructor.
-            //Console.WriteLine(obj.ToString());
-
-            // 2 Way : Instead of resolving the obj we resolve the facotry
-            // Resolve delegate factory that makes uses of container and Injects both dependency objs but also params not auto injected
-            var factory = container.Resolve<DomainObject.Factory>(); 
-            var obj = factory.Invoke(42);
-            Console.WriteLine(obj.ToString());
         }
     }
 }
+
