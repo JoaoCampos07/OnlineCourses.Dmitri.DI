3898d6569f8f6958a7d5ddb108f444d1815def89
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-09 10:16:56 +0100

    [Setup] ~ Create: Proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineCourses.Dmitri.DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
