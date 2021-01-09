627df95c52709caacc512ad0dfc02ae3461799d8
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-09 11:56:59 +0100

    [Section2] ~ Create: Proj

---
+++ AdvancedRegistrationConcepts/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace AdvancedRegistrationConcepts
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class EmailLog : ILog
+    {
+        private const string adminEmail = "admin@foo.com";
+        public void Write(string message)
+        {
+            Console.WriteLine($"Email sent to : {adminEmail} : {message}");
+        }
+    }
+
+    public class SMSLog : ILog
+    {
+        string phoneNumber;
+
+        public SMSLog(string phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine($"SMS to {phoneNumber} : {message}");
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            this.id = new Random().Next();
+        }
+
+        public Engine(ILog log, int id)
+        {
+            this.log = log;
+            this.id = id;
+        }
+
+        public void Ahead(int power)
+        {
+            log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private ILog log;
+        private Engine engine;
+
+        public Car(Engine engine)
+        {
+            this.engine = engine;
+            this.log = new EmailLog();
+        }
+
+        public Car(ILog log, Engine engine)
+        {
+            this.log = log;
+            this.engine = engine;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(200);
+            log.Write("Car going forward...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
