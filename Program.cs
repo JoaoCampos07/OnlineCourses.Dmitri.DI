03b4b81c8e3a01116559cf6b5792fd2bc879b7c4
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-09 10:33:53 +0100

    [Class1] + Add: Example without DI.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,62 @@
 
 namespace OnlineCourses.Dmitri.DI
 {
+    public interface ILog
+    {
+        void Write(string message); 
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            this.id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private ILog log;
+        private Engine engine;
+
+        public Car(ILog log, Engine engine)
+        {
+            this.log = log;
+            this.engine = engine;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(200);
+            log.Write("Car going forward...");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var log = new ConsoleLog();
+            var engine = new Engine(log);
+            var car = new Car(log, engine);
+            car.Go();
         }
     }
 }
