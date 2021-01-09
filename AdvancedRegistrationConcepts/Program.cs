a158bad472cab3c5d5a4ec08630bfa448d495a03
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-09 16:21:11 +0100

    [Class4] + Add: 3 solution - Making use Method injection

--- AdvancedRegistrationConcepts/Program.cs
+++ AdvancedRegistrationConcepts/Program.cs
@@ -14,6 +14,9 @@ namespace AdvancedRegistrationConcepts
     {
         public string Name { get; set; }
         public Parent Parent { get; set; }
+
+        public void SetParent(Parent parent)
+            => Parent = parent;
     }
 
      class Program
@@ -23,12 +26,22 @@ namespace AdvancedRegistrationConcepts
             var cb = new ContainerBuilder();
             cb.RegisterType<Parent>();
 
+            //Prop injection
             // 1 Way
             //cb.RegisterType<Child>().PropertiesAutowired(); // System is going to every prop and try to resolve it.
 
             // 2 Way 
-            cb.RegisterType<Child>()
-                .WithProperty("Parent", new Parent());
+            //cb.RegisterType<Child>()
+            //    .WithProperty("Parent", new Parent()); //it will always be the same Parent for every child
+
+            // Method injection
+            // 3 Way
+            cb.Register(c =>
+            {
+                var child = new Child();
+                child.SetParent(c.Resolve<Parent>());
+                return child;
+            });
 
             var container = cb.Build();
             var parent = container.Resolve<Child>().Parent;
