cd9fc9b13faa28543036c045142b7d0375f3f689
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-10 10:55:30 +0100

    [Class5] + Add: Specific registration that need specification.

--- AdvancedRegistrationConcepts/Program.cs
+++ AdvancedRegistrationConcepts/Program.cs
@@ -5,6 +5,27 @@ using System.Reflection;
 
 namespace AdvancedRegistrationConcepts
 {
+    public interface ILog
+    {
+        void Write(string message);
+    }
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class EmailLog : ILog
+    {
+        private const string adminEmail = "admin@foo.com";
+        public void Write(string message)
+        {
+            Console.WriteLine($"Email sent to : {adminEmail} : {message}");
+        }
+    }
+
     public class Parent
     {
         public override string ToString()
@@ -27,7 +48,10 @@ namespace AdvancedRegistrationConcepts
             var assembly = Assembly.GetExecutingAssembly();
             var builder = new ContainerBuilder();
             builder.RegisterAssemblyTypes(assembly)
-                .Where(t => t.Name.EndsWith("Log")); // I want to register only types that have a name that ends with Log
+                .Where(t => t.Name.EndsWith("Log")) // I want to register only types that have a name that ends with Log
+                .Except<EmailLog>() // I dont want EmailLog to be register and be given for service ILog
+                .Except<ConsoleLog>(c => c.As<ILog>().SingleInstance()) // I dont want to register ConsoleLog. I want it to be register this way...
+                .AsSelf(); // All types are register was components without service
 
         }
     }
