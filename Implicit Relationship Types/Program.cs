873ee6da3fc33c5a43db41f098d58a9ad7946a10
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-10 12:09:10 +0100

    [Section4] Implicit Relationship Types - setup proj

---
+++ Implicit Relationship Types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Implicit_Relationship_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
