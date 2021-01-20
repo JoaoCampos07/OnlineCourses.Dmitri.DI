f166c0615ee2c1e60a3a62d008f098184ce47b77
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-20 19:49:29 +0100

    [Class1] + Add: Presenting problem : How delegate to AUTOFAC job of registering missing component PERSON

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Autofac;
+using System;
 
 namespace AdvancedTopics
 {
+    public interface ICanSpeak
+    {
+        void Speak();
+    }
+
+    public class Person : ICanSpeak
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var b = new ContainerBuilder();
+
+            using (var c = b.Build())
+            {
+                c.Resolve<Person>().Speak();
+            }
         }
     }
 }
