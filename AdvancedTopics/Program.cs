3c9301635e50c61a2c0a29f55f144f73152cb30a
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-23 18:06:37 +0100

    [Class3] ~ Create: Class's/Objs that create Reports.

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -11,61 +11,16 @@ using System.Collections.Generic;
 
 namespace AdvancedTopics
 {
-    public interface ICommand
+    public interface IReportingService
     {
-        void Execute();
+        void Report();
     }
 
-    public class SaveCommand : ICommand
+    public class ReportingService : IReportingService
     {
-        public void Execute()
+        public void Report()
         {
-            Console.WriteLine("Saving a file.");
-        }
-    }
-
-    public class OpenCommand : ICommand
-    {
-        public void Execute()
-        {
-            Console.WriteLine("Opening a file");
-        }
-    }
-
-    // A button executes one command  
-    public class Button
-    {
-        private ICommand command;
-        private string name;
-
-        public Button(ICommand command, string name)
-        {
-            this.command = command;
-        }
-
-        public void OnClick()
-        {
-            command.Execute();
-        }
-
-        public void PrintMe() => Console.WriteLine($"I am a button called {name}");
-    }
-
-    public class Editor
-    {
-        private IEnumerable<Button> buttons;
-
-        public Editor(IEnumerable<Button> buttons)
-        {
-            this.Buttons = buttons;
-        }
-
-        public IEnumerable<Button> Buttons { get => buttons; private set => buttons = value; }
-
-        public void ClickAll()
-        {
-            foreach (var btn in Buttons)
-                btn.OnClick();
+            Console.WriteLine("Here is your report");
         }
     }
 
@@ -73,25 +28,6 @@ namespace AdvancedTopics
     {
         static void Main(string[] args)
         {
-            var b = new ContainerBuilder();
-            b.RegisterType<OpenCommand>().As<ICommand>()
-                .WithMetadata("Name", "Open");
-            b.RegisterType<SaveCommand>().As<ICommand>()
-                .WithMetadata("Name", "Save");
-
-            // Let's specifiy the name of the button with Metadata
-            b.RegisterAdapter<Meta<ICommand>, Button>(cmd => new Button(cmd.Value, (string)cmd.Metadata["Name"]));
-            b.RegisterType<Editor>();
-
-            using (var c = b.Build())
-            {
-                var editor = c.Resolve<Editor>();
-
-                foreach (var btn in editor.Buttons)
-                {
-                    btn.PrintMe();
-                }
-            }
         }
     }
 }
