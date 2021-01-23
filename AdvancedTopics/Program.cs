4492ca5e67b274aa76d87377c790dca10bb37cfc
JoaoPedroQCampos <joao.campos@itsector.pt>
2021-01-23 18:17:42 +0100

    [Class3] + Add: Presenting problem, injecting other implementation of IReportingService in the decorator.

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -24,7 +24,7 @@ namespace AdvancedTopics
         }
     }
 
-    //Decorator style
+    //Typical Decorator - reuses another service (that also implements IReportingService and DECORATES IT with reporting functionality)
     // Logger to log the initialize of reporting and the end of the reporting
     public class ReportingServiceWithLogging : IReportingService
     {
@@ -47,6 +47,17 @@ namespace AdvancedTopics
     {
         static void Main(string[] args)
         {
+            // Can the Autofac provider the right components whem we implement the Decorator pattern ? 
+            var b = new ContainerBuilder();
+            b.RegisterType<ReportingServiceWithLogging>().As<IReportingService>();
+            // Like it is above, it will end up in infinte loop or throught exception, because the reportingServiceWithLogging component
+            // needs a IReportingService, so...Autofac understands that he needs a instance of itself. (ReportingServiceWIthLogging is as Service IReportingService)
+            // And it sees that the instance that it needs (himself) needs also a IReportingService and so on, and so on, in a infinite loop.
+
+            using (var c = b.Build())
+            {
+                c.Resolve<IReportingService>().Report();
+            }
         }
     }
 }
